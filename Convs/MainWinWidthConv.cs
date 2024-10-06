4d4b43284c04babc267520263669da9b3a5068ac
Space Time <Zeus6_6@163.com>
2024-10-06 17:39:47 +0800

    1.1.1 -> 1.1.2 第39次更新

---
+++ Convs/MainWinWidthConv.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Sheas_Cealer.Convs;
+
+internal class MainWinWidthConv : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        bool isAdmin = (bool)value;
+
+        return isAdmin ? 700 : 500;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
