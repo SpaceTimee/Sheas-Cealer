372d9002809ba1ccd9f74b316998fdcb3b55fb0c
Space Time <Zeus6_6@163.com>
2024-11-12 11:58:44 +0800

    1.1.3 -> 1.1.4 第3次更新

--- Consts/AboutConst.cs
+++ Consts/AboutConst.cs
@@ -5,7 +5,7 @@ namespace Sheas_Cealer.Consts;
 internal class AboutConst : AboutMultilangConst
 {
     public static string DeveloperButtonUrl => "https://www.spacetimee.xyz";
-    public static string VersionButtonVersionContent => Assembly.GetExecutingAssembly().GetName().Version!.ToString()[0..^2];
+    public static string VersionButtonVersionContent => Assembly.GetExecutingAssembly().GetName().Version!.ToString()[..^2];
     public static string VersionButtonUrl => "https://spacetime.lanzouu.com/b017hp0lc";
     public static string EmailButtonUrl => "Zeus6_6@163.com";
 
