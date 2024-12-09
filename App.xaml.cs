73f411aca30f65d47a025659bd906b9f3688a671
Space Time <Zeus6_6@163.com>
2024-12-09 22:11:42 +0800

    1.1.3 -> 1.1.4 第38次更新

--- App.xaml.cs
+++ App.xaml.cs
@@ -17,25 +17,31 @@ public partial class App : Application
     {
         _ = new SettingsPres();
 
+        #region Primary Color
         PaletteHelper paletteHelper = new();
         Theme newTheme = paletteHelper.GetTheme();
         System.Drawing.Color newColor = Settings.Default.PrimaryColor;
 
         newTheme.SetPrimaryColor(Color.FromRgb(newColor.R, newColor.G, newColor.B));
         paletteHelper.SetTheme(newTheme);
+        #endregion Primary Color
 
+        #region Background Color
         if (Environment.OSVersion.Version.Build < 22000)
         {
             Style newWindowStyle = new(typeof(Window), Current.Resources["CommonWindow"] as Style);
             newWindowStyle.Setters.Add(new Setter(Window.BackgroundProperty, new DynamicResourceExtension("MaterialDesignBackground")));
             Current.Resources["CommonWindow"] = newWindowStyle;
         }
+        #endregion Background Color
 
+        #region Foreground Color
         Color? foregroundColor = ForegroundGenerator.GetForeground(newColor.R, newColor.G, newColor.B);
 
         Style newButtonStyle = new(typeof(Button), Current.Resources[typeof(Button)] as Style);
         newButtonStyle.Setters.Add(new Setter(Button.ForegroundProperty, foregroundColor.HasValue ? new SolidColorBrush(foregroundColor.Value) : new DynamicResourceExtension("MaterialDesignBackground")));
         Current.Resources[typeof(Button)] = newButtonStyle;
+        #endregion Foreground Color
 
         new MainWin(e.Args).Show();
     }
